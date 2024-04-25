6e2a70404863427cdf12df0ebcb335b622e72007
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-25 17:01:15 -0400

    Doing Examples

---
+++ Exercises/conversion_tests.js
@@ -0,0 +1,22 @@
+// Inputs
+
+let a = "  101.1   ";
+let b = "55";
+let c = "402 Stevents";
+let d = " Number 5 "
+
+
+
+// Outputs
+
+let pI_a = parseInt(a);
+let pF_a = parseFloat(a);
+let num_a = Number(a);
+let una_a = +a;
+
+// console.log(pI_a, pF_a, num_a, una_a)
+
+console.log(parseInt(a), parseFloat(a), Number(a), +a)
+console.log(parseInt(b), parseFloat(b), Number(b), +b)
+console.log(parseInt(c), parseFloat(c), Number(c), +c)
+console.log(parseInt(d), parseFloat(d), Number(d), +d)
